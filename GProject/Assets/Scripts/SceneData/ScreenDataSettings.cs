984ea69ec32002d2c98e23af5d430ff1b5fe6091
PULK4N <nikola_s99@live.com>
2019-08-22 23:00:30 +0200

    Game menu add
    Game menu added

---
+++ GProject/Assets/Scripts/SceneData/ScreenDataSettings.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+/// <summary>
+/// Persistent for all game saves, long term storage
+/// </summary>
+public class SceneDataSettings
+{
+    public string Version = "0.1";
+    public string Filename = "settings.dat";
+    public string Lastsave = "save1.dat";
+
+    #region Save/Load
+    public void Save()
+    {
+        SceneDataWriter.Save(GameManager.Instance.DataPath + Filename, this);
+    }
+
+    public SceneDataSettings Load()
+    {
+        var settings = SceneDataWriter.Load<SceneDataSettings>(GameManager.Instance.DataPath + Filename);
+
+        if (settings != null)
+            return settings;
+
+        return this;
+    }
+    #endregion
+}
