720022e062cc6070fbfd94394586597e214f0b3e
PULK4N <nikola_s99@live.com>
2019-08-24 23:51:49 +0200

    Started working with network HUD manager
    Implemented bassics of HUD manager
    
    //Play button has stoppend working, probobly because of the network

---
+++ GProject/Assets/Scripts/GameManagerEvents.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerEvents : MonoBehaviour
+{
+    public delegate void GameModeChanged();
+    public event GameModeChanged DoGameModeChanged;
+}
