314b9f0209ab2a67759afb837544900e1bf5a751
PULK4N <nikola_s99@live.com>
2019-08-26 00:14:47 +0200

    LevelManager script added
    Added and edited some scripts like LevelManager,GameManager,Enums, MainMenu and BattleZone edited.
    Player prefab added to net

--- GProject/Assets/Scripts/GameManagerEvents.cs
+++ GProject/Assets/Scripts/GameManagerEvents.cs
@@ -1,10 +1,15 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GameManagerEvents : MonoBehaviour
 {
-    public delegate void GameModeChanged();
+    public delegate void GameModeChanged(Enums.GameStates state);
     public event GameModeChanged DoGameModeChanged;
+
+    public void InvokeGameModeChanged(Enums.GameStates state)
+    {
+        if(DoGameModeChanged != null)
+        {
+            DoGameModeChanged(state);
+        }
+    }
 }
