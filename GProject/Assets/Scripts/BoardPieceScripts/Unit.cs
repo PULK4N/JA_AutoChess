d5da0b8fdb7795b6a28cafdb5d4cea7de0a332e3
PULK4N <nikola_s99@live.com>
2019-08-27 01:24:54 +0200

    Base clases for figures etc. created

---
+++ GProject/Assets/Scripts/BoardPieceScripts/Unit.cs
@@ -0,0 +1,17 @@
+﻿public abstract class Unit
+{
+    struct Properties
+    {
+        float Health;
+        float Mana;
+        float Range;
+        float AttackDamage;
+        float AbilityDamage;
+        float LifeSteal;
+        float SpellWamp;
+        float Shield;
+        bool IsInvounrable;
+        bool ManaBurn;
+    }
+    public Ability Ability;
+}
